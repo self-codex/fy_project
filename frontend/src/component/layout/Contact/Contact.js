50e3ed4ee323ea8930172ce4b0c7fc36a3350645
Mohammed Talha <self-codex@users.noreply.github.com>
2021-10-08 10:28:45 +0500

    final year project complete

---
+++ frontend/src/component/layout/Contact/Contact.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Contact.css";
+import { Button } from "@material-ui/core";
+
+const Contact = () => {
+  return (
+    <div className="contactContainer">
+      <a className="mailBtn" href="mcsw-f19-048@superior.edu.pk">
+        <Button>Contact: mcsw-f19-048@superior.edu.pk</Button>
+      </a>
+    </div>
+  );
+};
+
+export default Contact;
